5a6de72846f6fd0d15ec44e9bcab252dee22af21
Laurenz Soltau <soltau@campus.tu-berlin.de>
2025-03-30 20:41:56 +0200

    Write todoManagerModel

--- src/js/projectModel.js
+++ src/js/projectModel.js
@@ -1,21 +1,24 @@
 const createProject = function(name) {
     let projectName = name;
+    const projectId = crypto.randomUUID();
     const todos = [];
 
     const getName = () => projectName; 
     const setName = newName => projectName = newName;
+    const getId = () => projectId;
 
     const getTodos = () => todos; 
     const getTodo = id => todos.find(todo => todo.id == id);
     const addTodo = todo => todos.push(todo);
-    const removeTodo = todo => {
-        const index = todos.findIndex(_todo => _todo === todo);
+    const removeTodo = todoId => {
+        const index = todos.findIndex(todo => todo.id === todoId);
         todos.splice(index, 1);
     };
 
     return {
         getName,
         setName,
+        getId,
         getTodos,
         getTodo,
         addTodo,
--- src/js/todoManagerModel.js
+++ src/js/todoManagerModel.js
@@ -1,4 +1,46 @@
-import { createTodo } from "./todoModel.js";
 import { createProject } from "./projectModel.js";
 
+const todoManager = (function () {
+    const todos = [];
+    const projects = [];
 
+    const addProject = function (project) {
+        projects.push(project);
+    };
+
+    const removeProject = function (projectId) {
+        const project = projects.find(
+            (project) => project.getId() === projectId
+        );
+        const index = projects.findIndex(
+            (project) => project.getId() === projectId
+        );
+        for (const todo of project.getTodos()) {
+            removeTodo(todo.id, projectId);
+        }
+    };
+
+    const addTodo = function (todo, projectId) {
+        todos.push(todo);
+        const project = projects.find(
+            (project) => project.getId() === projectId
+        );
+        project.addTodo(todo);
+    };
+
+    const removeTodo = function (todoId, projectId) {
+        const index = todos.findIndex((todo) => todo.id === todoId);
+        todos.splice(index, 1);
+        const project = projects.find(
+            (project) => project.getId() === projectId
+        );
+        project.removeTodo(todoId);
+    };
+
+    const printTodos = function () {
+        console.log(todos);
+        console.log(projects);
+    };
+})();
+
+export default todoManager;
